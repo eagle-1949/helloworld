3cbb10ed4716ec9919d16619c091fca0bb0dfb91
longfengfirst <long_608@126.com>
2016-05-25 16:27:28 +0800

    Update HelloWorld.java

--- src/cn/longfirst/HelloWorld.java
+++ src/cn/longfirst/HelloWorld.java
@@ -1,6 +1,8 @@
 package cn.longfirst;
 public class HelloWorld {
-public static void main(String[] args) {
-System.out.println("Hello World");
-}
+  public static void main(String[] args) {
+    System.out.println("Author:Hello");
+    System.out.println("Hello World");
+    System.out.println("DBName:lancaihe");
+  }
 }
