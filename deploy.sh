12a4290d4806df0073a796513f5a4fac4e230e6a
longfengfirst <long_608@126.com>
2016-05-25 16:32:31 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo 'Hello World'
-ant 
+#原始文件
+cat src/cn/longfirst/HelloWorld.java
+sed -i 's/lancaihe/lancaihe2016/g' src/cn/longfirst/HelloWorld.java
+#变更后文件
+cat src/cn/longfirst/HelloWorld.java
+#ant 
